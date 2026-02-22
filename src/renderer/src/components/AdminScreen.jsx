3f028c69469e6c9292cbcc3c249696a57ae9d65e
ChangkookChoi <changkook94@gmail.com>
2026-02-22 14:36:03 +0900

    for deployment

--- src/renderer/src/components/AdminScreen.jsx
+++ src/renderer/src/components/AdminScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { isElectron } from '../utils/env';
 
+
 const AdminScreen = ({ onBack }) => {
   const [printers, setPrinters] = useState([]);
   const [selectedPrinter, setSelectedPrinter] = useState('');
